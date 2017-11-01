21c7367b17e56e7ce9fdd36f8c260e77003f8487
Adrien M <adrien@d.tube>
2017-11-01 21:54:10 +0100

    add express and stuff

--- encrypt.js
+++ encrypt.js
@@ -1,10 +1,10 @@
 var base64Img = require('base64-img');
 var steem = require('steem');
 
-var senderPublicKey = 'STM5hL457wPDtMY8ZySphuD9omZDnZHihMj1BDTkE1o6mDCmQSuMJ'
-var senderPrivateKey = '5JRA45dxxPDJntrR7DJYU5yRpRyqTCSw35Mvzag3g5RLPmQvVfz'
+var senderPublicKey = 'STM8PTfBzwLq38cvX3TJiYExSTMtHBvMAf1YSXkCu3G5Hh3xBkbqQ'
+var senderPrivateKey = '5JHjZc4nt2Lp929pcXRs39RkjvMmNYY2a4phMU3by8uUs8uWoqC'
 var receiverPublicKey = 'STM7s8o9v3Jyo722PLKtSvKffVBPUVNaGD4gVAGd1hk28dV3MHUhB'
-var senderPrivatePostingKey = '5JCKHs9S8FSE98ZQ3grmtQCyS7w87gPkEm5kfpvqsSNe6mcH8M4'
+var senderPrivatePostingKey = '5KgksXeg4rNvVoJjRJ5ve7RcxpPww1v8V8pKyDFV69j6cPrkgm5'
 var sender = 'curator'
 
 var rootPost = {
@@ -28,16 +28,16 @@ base64Img.base64('/home/adr/Images/ass.jpg', function(err, data) {
 		rootPost.author,
 		rootPost.permlink,
 		sender,
-		'jqefoiqefoijqoijef123',
+		'1uifhui1hfi1h3fio13oih',
 		'Second Test',
 		'We might use the body to mention people and put a link',
 		jsonMetadata,
 		function(err, result) {
 
 
+			if (err) console.log('error')
+			else console.log('done')
 
-
-			console.log(err, result)
 		}
 	);
 })
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const hash = require('hash.js')
+
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const app = express()
+ 
+app.use(fileUpload())
+
+app.get('/', function (req, res) {
+  
+})
+ 
+app.post('/upload', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.')
+ 
+  // The name of the input field (i.e. "encryptedFile") is used to retrieve the uploaded file
+  var encryptedFile = req.files.encryptedFile
+ 
+  // Use the mv() method to place the file somewhere on your server
+  encryptedFile.mv('/home/adr/Coding/hackaton/encpic/uploads', function(err) {
+    if (err)
+      return res.status(500).send(err)
+ 
+    res.send('File uploaded!')
+  })
+})
