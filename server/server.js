967f8abf4a6b9cce77a8cf809dbb2a744ccc7a10
Adrien M <adrien@d.tube>
2017-11-01 22:20:14 +0100

    server side code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-const hash = require('hash.js')
-
+const port = process.argv[2]
+const path = require('path');
+const sha512 = require('hash.js/lib/hash/sha/512');
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const app = express()
@@ -7,7 +8,7 @@ const app = express()
 app.use(fileUpload())
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/client/index.html'));
+  res.sendFile(path.join(__dirname,'../client/index.html'));
 })
  
 app.post('/upload', function(req, res) {
@@ -17,11 +18,16 @@ app.post('/upload', function(req, res) {
   // The name of the input field (i.e. "encryptedFile") is used to retrieve the uploaded file
   var encryptedFile = req.files.encryptedFile
  
+  // calculate the sha512 of the file
+  var sha512 = sha512().update('abc').digest('hex')
+
   // Use the mv() method to place the file somewhere on your server
-  encryptedFile.mv('/uploads', function(err) {
+  encryptedFile.mv('/uploads/'+sha512, function(err) {
     if (err)
       return res.status(500).send(err)
  
-    res.send('File uploaded!')
+    res.send(sha512)
   })
 })
+
+app.listen(port);
