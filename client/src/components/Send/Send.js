85b2f31665b1af27170c164361716c3aecb48d65
Pedro Reis Colaço <pedro@Pedros-MacBook-Pro.local>
2017-11-02 00:17:30 +0000

    Send file layout

--- client/src/components/Send/Send.js
+++ client/src/components/Send/Send.js
@@ -1,6 +1,10 @@
 // @flow
 import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap';
+import {
+  Panel,
+  Form, FormGroup, InputGroup, FormControl,
+  ButtonToolbar, Button
+} from 'react-bootstrap';
 
 import './Send.css';
 
@@ -10,15 +14,67 @@ type Props = {
 };
 
 type State = {
+  sender: string,
+  key: string,
+  receiver: string,
+
 };
 
 export default class Send extends Component<Props, State> {
   state = {
+    sender: '',
+    key: '',
+    receiver: '',
+    file: '',
+    valid: false
   };
 
-  constructor(props: Props) {
-    super(props);
+  file = null;
 
+
+  changeSender(e) {
+    const sender = e.target.value;
+    this.setState({sender});
+    this.validate();
+  }
+
+  changeSenderKey(e) {
+    const key = e.target.value;
+    this.setState({key});
+    this.validate();
+  }
+
+  changeReceiver(e) {
+    const receiver = e.target.value;
+    this.setState({receiver});
+    this.validate();
+  }
+
+  clickFile() {
+    this.performClick(this.file);
+  }
+
+  performClick(elem) {
+    if(elem && document.createEvent) {
+      var evt = document.createEvent("MouseEvents");
+      evt.initEvent("click", true, false);
+      elem.dispatchEvent(evt);
+    }
+  }
+
+  changeFile(e) {
+    const file = e.target.value;
+    this.setState({file});
+    this.validate();
+  }
+
+  validate() {
+    console.log(this.state)
+    if(this.state.sender.length >= 3 && this.state.receiver.length >= 3 &&
+        this.state.key.length >= 10 && this.state.file !== '') {
+      this.setState({valid: true});
+    }
+    this.setState({valid: false});
   }
 
   render() {
@@ -29,7 +85,46 @@ export default class Send extends Component<Props, State> {
     return (
       <div className="background">
         <Panel header={title} className="panel">
-          Hello
+          <Form>
+            <FormGroup controlId="formSender">
+              <InputGroup>
+                <InputGroup.Addon><i className="send-icon fa fa-user" /></InputGroup.Addon>
+                <FormControl type="text" placeholder="Sender STEEM Username"
+                  onChange={this.changeSender.bind(this)}
+                />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup controlId="formSenderKey">
+              <InputGroup>
+                <InputGroup.Addon><i className="send-icon fa fa-key" /></InputGroup.Addon>
+                <FormControl type="password" placeholder="Sender STEEM Private Posting Key"
+                  onChange={this.changeSenderKey.bind(this)}
+                />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup controlId="formReceiver">
+              <InputGroup>
+                <InputGroup.Addon><i className="send-icon fa fa-user-secret" /></InputGroup.Addon>
+                <FormControl type="text" placeholder="Receiver STEEM Username"
+                  onChange={this.changeReceiver.bind(this)}
+                />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup controlId="formFile">
+              <InputGroup>
+                <InputGroup.Button>
+                  <Button onClick={this.clickFile.bind(this)}>Select CKRET File</Button>
+                </InputGroup.Button>
+                <FormControl value={this.state.file !== '' ? this.state.file : 'No CKRET file'} readOnly />
+              </InputGroup>
+              <input type="file" ref={elem => this.file = elem} className="hidden" onChange={this.changeFile.bind(this)} />
+            </FormGroup>
+            <FormGroup>
+              <ButtonToolbar className="pull-right">
+                <Button bsStyle="primary" disabled={!this.state.valid}>Send it</Button>
+              </ButtonToolbar>
+            </FormGroup>
+          </Form>
         </Panel>
       </div>
     );
